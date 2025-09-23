c3d9aec682f3551c8682b766b1161e70c28ed8df
myron302 <myron302@gmail.com>
2025-09-23 10:46:15 -0400

    Add users and posts routes to the API

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -5,19 +5,17 @@ import recipes from "./recipes";
 import substitutions from "./substitutions";
 import pantry from "./pantry";
 import marketplace from "./marketplace";
+import users from "./users";
+import posts from "./posts";
 
 const router = Router();
 
-// Grouped API under /api/*
+// /api/*
 router.use("/recipes", recipes);
 router.use("/substitutions", substitutions);
 router.use("/pantry", pantry);
 router.use("/marketplace", marketplace);
-
-// You can add more here next:
-// import users from "./users";
-// import posts from "./posts";
-// router.use("/users", users);
-// router.use("/posts", posts);
+router.use("/users", users);
+router.use("/posts", posts);
 
 export default router;
