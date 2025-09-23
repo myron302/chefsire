4ef0521314427de57346f95ee019bb9da5d9d2f8
myron302 <myron302@gmail.com>
2025-09-23 10:35:25 -0400

    Add pantry and marketplace routes to the API

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,12 +1,23 @@
 // server/routes/index.ts
 import { Router } from "express";
+
 import recipes from "./recipes";
 import substitutions from "./substitutions";
+import pantry from "./pantry";
+import marketplace from "./marketplace";
 
 const router = Router();
 
-// group endpoints
+// Grouped API under /api/*
 router.use("/recipes", recipes);
 router.use("/substitutions", substitutions);
+router.use("/pantry", pantry);
+router.use("/marketplace", marketplace);
+
+// You can add more here next:
+// import users from "./users";
+// import posts from "./posts";
+// router.use("/users", users);
+// router.use("/posts", posts);
 
 export default router;
