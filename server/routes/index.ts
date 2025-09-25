c06f78406304a76110cc714028bfc7efc7f02005
myron302 <myron302@gmail.com>
2025-09-25 18:05:36 -0400

    Integrate drinksRouter into main API routes
    Added drinksRouter to the main router for API.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -8,7 +8,35 @@ import usersRouter from "./users";
 import postsRouter from "./posts";
 import pantryRouter from "./pantry";
 import marketplaceRouter from "./marketplace";
+import substitutionsRouter from "./substitutions";// server/routes/index.ts
+import { Router } from "express";
+
+// Existing routers
+import recipesRouter from "./recipes";
+import bitesRouter from "./bites";
+import usersRouter from "./users";
+import postsRouter from "./posts";
+import pantryRouter from "./pantry";
+import marketplaceRouter from "./marketplace";
 import substitutionsRouter from "./substitutions";
+
+// NEW: drinks
+import drinksRouter from "./drinks";
+
+const r = Router();
+
+// Mounted under /api in app.ts
+r.use(recipesRouter);
+r.use(bitesRouter);
+r.use(usersRouter);
+r.use(postsRouter);
+r.use(pantryRouter);
+r.use(marketplaceRouter);
+r.use(substitutionsRouter);
+r.use(drinksRouter);
+
+export default r;
+
 
 const r = Router();
 
