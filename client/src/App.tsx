2344ed3e2af9a6fd7fcd0aae8f45ddeb319156c1
myron302 <myron302@gmail.com>
2025-10-02 16:55:59 -0400

    Update detox routes to include drinks prefix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -129,10 +129,11 @@ function DrinksSection() {
       <Route path="/drinks/protein-shakes" component={ProteinShakesHub} />
 
       {/* ========== DETOXES ROUTES ========== */}
-<Route path="/detoxes/juice" component={DetoxJuices} />
-<Route path="/detoxes/tea" component={DetoxTeas} />
-<Route path="/detoxes/water" component={DetoxWaters} />
-<Route path="/detoxes" component={DetoxesHub} />
+      {/* ✅ FIXED: Added correct /drinks prefix */}
+      <Route path="/drinks/detoxes/juice" component={DetoxJuices} />
+      <Route path="/drinks/detoxes/tea" component={DetoxTeas} />
+      <Route path="/drinks/detoxes/water" component={DetoxWaters} />
+      <Route path="/drinks/detoxes" component={DetoxesHub} />
 
       {/* ========== POTENT POTABLES ROUTES (AGE-GATED) ========== */}
       <Route path="/drinks/potent-potables/:rest*" component={PotentPotablesSection} />
