d123c506856dab207580200378052c300979ad25
myron302 <myron302@gmail.com>
2025-10-04 09:46:41 -0400

    Replace Next.js Link with React Router Link

--- client/src/pages/drinks/protein-shakes/index.tsx
+++ client/src/pages/drinks/protein-shakes/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
+import { Link } from 'react-router-dom';
 import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
