29221aa798e99152f4adedce5a6925e592bda11a
myron302 <myron302@gmail.com>
2025-10-11 17:42:07 -0400

    Fix import path for queryClient

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Switch, Route, useLocation } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 
-import { queryClient } from "@/lib/query-client";
+import { queryClient } from "@/lib/queryClient";
 
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
