0b0f34054b9af385702cedfbb31a8e7201b8745a
myron302 <myron302@gmail.com>
2025-10-12 11:39:27 -0400

    Add load-env.ts to load environment variables

---
+++ server/lib/load-env.ts
@@ -0,0 +1,13 @@
+import { createRequire } from "node:module";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const require = createRequire(import.meta.url);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+if (!process.env.DATABASE_URL) {
+  const envPath = path.resolve(__dirname, "../.env"); // /httpdocs/server/.env
+  try {
+    require("dotenv").config({ path: envPath });
+  } catch {}
+}
