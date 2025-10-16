18137201e97b8ca75ef146b388177951fa471868
myron302 <myron302@gmail.com>
2025-10-16 13:12:21 -0400

    Add mocktails option to drinks list

--- client/src/pages/drinks/potent-potables/vodka/index.tsx
+++ client/src/pages/drinks/potent-potables/vodka/index.tsx
@@ -396,6 +396,7 @@ const sisterPotentPotablesPages = [
   { id: 'daiquiri', name: 'Daiquiri', path: '/drinks/potent-potables/daiquiri', icon: Droplets, description: 'Rum classics' },
   { id: 'scotch', name: 'Scotch & Irish', path: '/drinks/potent-potables/scotch-irish-whiskey', icon: Wine, description: 'UK whiskeys' },
   { id: 'classic', name: 'Classic Cocktails', path: '/drinks/potent-potables/cocktails', icon: Wine, description: 'Timeless recipes' },
+  { id: 'mocktails', name: 'Mocktails', path: '/drinks/potent-potables/mocktails', icon: Sparkles, description: 'Zero-proof' },
   { id: 'seasonal', name: 'Seasonal', path: '/drinks/potent-potables/seasonal', icon: Sparkles, description: 'Festive drinks' }
 ];
 
