05a4147bf8258c22e6d755659fd852ad6291cce4
myron302 <myron302@gmail.com>
2025-10-16 12:54:39 -0400

    Add Mocktails option to drinks list

--- client/src/pages/drinks/potent-potables/martinis/index.tsx
+++ client/src/pages/drinks/potent-potables/martinis/index.tsx
@@ -312,6 +312,7 @@ const sisterPotentPotablesPages = [
   { id: 'scotch', name: 'Scotch & Irish', path: '/drinks/potent-potables/scotch-irish-whiskey', icon: Wine, description: 'UK whiskeys' },
   { id: 'classic', name: 'Classic Cocktails', path: '/drinks/potent-potables/cocktails', icon: Wine, description: 'Timeless recipes' },
   { id: 'seasonal', name: 'Seasonal', path: '/drinks/potent-potables/seasonal', icon: Sparkles, description: 'Festive drinks' }
+  { id: 'mocktails', name: 'Mocktails', path: '/drinks/potent-potables/mocktails', icon: Sparkles, description: 'Zero-proof' }
 ];
 
 // CROSS-HUB
