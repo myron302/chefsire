066bf5e305a0d643fd3fd49597eb0bebe955a81a
myron302 <myron302@gmail.com>
2025-10-21 18:29:35 -0400

    Fix import path for stores in stores.ts

--- server/routes/stores.ts
+++ server/routes/stores.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { db } from "../db";
-import { stores } from "../../shared/schema";
+import { stores } from "../db";
 import { eq } from "drizzle-orm";
 
 const router = Router();
