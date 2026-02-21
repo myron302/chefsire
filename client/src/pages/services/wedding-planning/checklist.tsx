c4f41108dde1dbc612ec349ec4ffc3e70dc1f7da
myron302 <myron302@gmail.com>
2026-02-20 19:39:14 -0500

    Add WeddingPlanningChecklistPage component

---
+++ client/src/pages/services/wedding-planning/checklist.tsx
@@ -0,0 +1,5 @@
+import { WeddingPlanningWorkspace } from "../wedding-planning";
+
+export default function WeddingPlanningChecklistPage() {
+  return <WeddingPlanningWorkspace mode="checklist" />;
+}
