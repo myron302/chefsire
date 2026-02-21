a56e6b8bce6411eb477c9365c3613a61c594e545
myron302 <myron302@gmail.com>
2026-02-20 19:39:40 -0500

    Add WeddingPlanningInvitationsPage component

---
+++ client/src/pages/services/wedding-planning/invitations.tsx
@@ -0,0 +1,5 @@
+import { WeddingPlanningWorkspace } from "../wedding-planning";
+
+export default function WeddingPlanningInvitationsPage() {
+  return <WeddingPlanningWorkspace mode="invitations" />;
+}
