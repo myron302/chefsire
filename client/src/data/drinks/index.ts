4477446a8a4e3af63e22480877d01dc22e169115
myron302 <myron302@gmail.com>
2026-03-03 14:53:10 -0500

    Migrate gin page to drink data registry and expand recipes (#720)

---
+++ client/src/data/drinks/index.ts
@@ -0,0 +1,9 @@
+import { ginRecipes } from "./potent-potables/gin";
+
+export const drinkRouteRegistry = [
+  {
+    route: "/drinks/potent-potables/gin",
+    title: "Gin",
+    recipes: ginRecipes
+  }
+];
