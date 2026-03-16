71aa85a04ffaa24604cc04bf8e9e5dc50f1152b1
myron302 <myron302@gmail.com>
2026-03-16 15:09:10 -0400

    Remove duplicate unused service and explore page files

--- client/src/pages/services/invitations.tsx
+++
@@ -1,5 +0,0 @@
-import { WeddingPlanningWorkspace } from "../wedding-planning";
-
-export default function WeddingPlanningInvitationsPage() {
-  return <WeddingPlanningWorkspace mode="invitations" />;
-}
--- client/src/pages/services/vendors.tsx
+++
@@ -1,5 +0,0 @@
-import { WeddingPlanningWorkspace } from "../wedding-planning";
-
-export default function WeddingPlanningVendorsPage() {
-  return <WeddingPlanningWorkspace mode="vendors" />;
-}
